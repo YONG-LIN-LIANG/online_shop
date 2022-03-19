c806114c02602b5cfc5ee89f774e4e9f947dbb90
Steven <steven@gmail.com>
2022-03-19 14:44:29 +0800

    feat: 安裝carousel套件，並建立BannerCarousel套件

---
+++ components/index/BannerCarousel.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+		<VueSlickCarousel v-bind="settings">
+			<div>
+				<img src="" alt="">
+			</div>
+		</VueSlickCarousel>
+	</section>
+</template>
+
+<script>
+	import VueSlickCarousel from 'vue-slick-carousel'
+	export default {
+		components: {
+			VueSlickCarousel
+		},
+		data(){
+			return{
+				settings: {
+					"dots": true,
+					"dotsClass": "slick-dots custom-dot-class",
+					"edgeFriction": 0.35,
+					"infinite": false,
+					"speed": 500,
+					"slidesToShow": 1,
+					"slidesToScroll": 1
+				}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <section>
+    <BannerCarousel/>
   </section>
 </template>
 
 <script>
+import BannerCarousel from '~/components/index/BannerCarousel'
 export default {
   components: {
+    BannerCarousel
   }
 }
 </script>
